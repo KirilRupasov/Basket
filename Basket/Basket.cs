7142ddabdff9ba9dd3ff2c2dca44ae65a1bd9375
Kiril Rupasov <kirilrupasov@gmail.com>
2022-02-12 14:20:47 +0000

    Initialised example Basket class

---
+++ Basket/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Basket
+{
+    public class Basket
+    {
+        private decimal _totalCost { get; set; }
+        
+        public Basket() {
+            _totalCost = 0.0M;
+        }
+        
+        public void AddToCart(decimal productPrice) {
+            _totalCost += productPrice;
+        }
+        
+        public decimal GetTotalCost() {
+            return _totalCost;
+        }
+    }
+}
