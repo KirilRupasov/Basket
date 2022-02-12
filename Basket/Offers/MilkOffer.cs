db416eebcbcd74872f74106299aff33736dc9b8a
Kiril Rupasov <kirilrupasov@gmail.com>
2022-02-12 15:47:41 +0000

    fixed the milk offer - test works!

--- Basket/Offers/MilkOffer.cs
+++ Basket/Offers/MilkOffer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,8 +9,21 @@ namespace Basket.Offers
         public void ApplyOffer(IList<IProduct> products)
         {
             if (products.OfType<Milk>().Count() < 4) return;
-            
-            products.OfType<Milk>().FirstOrDefault()?.SetPrice(0.00M);
+
+            var milkProducts = products.OfType<Milk>();
+
+            var i = 1;
+
+            foreach (var milk in milkProducts)
+            {
+                if (i == 4)
+                {
+                    milk.SetPrice(0.00M);
+                    i = 1;
+                }
+                
+                i++;
+            }
         }
     }
 }
