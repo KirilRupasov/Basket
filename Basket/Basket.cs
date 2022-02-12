c2ce454bb43b645b21d9179a3e5b2b05f57baf6d
Kiril Rupasov <kirilrupasov@gmail.com>
2022-02-12 14:49:39 +0000

    now we add products to Basket and then calculate the sum -> tests still run with the same outcome

--- Basket/Basket.cs
+++ Basket/Basket.cs
@@ -1,21 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Basket
 {
     public class Basket
     {
         private decimal _totalCost { get; set; }
+        private IList<IProduct> _products { get; set; }
         
         public Basket() {
             _totalCost = 0.0M;
+            _products = new List<IProduct>();
         }
         
         public void AddToCart(IProduct product) {
+            _products.Add(product);
             _totalCost += product.GetPrice();
         }
         
-        public decimal GetTotalCost() {
-            return _totalCost;
+        public decimal GetTotalCost()
+        {
+            return _products.Select(x => x.GetPrice()).Sum();
         }
     }
 }
