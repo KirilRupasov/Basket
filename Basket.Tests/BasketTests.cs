2d3e6a16c418bc8e00a11911836d4668640c9a8e
Kiril Rupasov <kirilrupasov@gmail.com>
2022-02-12 14:28:02 +0000

    Added first unit tests - passes!

--- Basket.Tests/BasketTests.cs
+++ Basket.Tests/BasketTests.cs
@@ -13,9 +13,20 @@ namespace Basket.Tests
         }
 
         [Test]
-        public void Test1()
+        public void WhenBasketHasOneBreadOneMilkOneButter_ThenTotalPriceShouldBeTwoNinetyFive()
         {
-            Assert.Pass();
+            var milkCost = 1.15M;
+            var butterCost = 0.80M;
+            var breadCost = 1.00M;
+            
+            _basket.AddToCart(milkCost);
+            _basket.AddToCart(butterCost);
+            _basket.AddToCart(breadCost);
+
+            var expectedTotalPrice = 2.95M;
+            var actualTotalPrice = _basket.GetTotalCost();
+            
+            Assert.AreEqual(expectedTotalPrice, actualTotalPrice);
         }
     }
 }
