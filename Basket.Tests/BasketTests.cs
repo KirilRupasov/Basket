dfc9e017528d01628aa538a8b18c9e4af6ed59aa
Kiril Rupasov <kirilrupasov@gmail.com>
2022-02-12 14:22:17 +0000

    did some basic setup

--- Basket.Tests/BasketTests.cs
+++ Basket.Tests/BasketTests.cs
@@ -2,11 +2,14 @@ using NUnit.Framework;
 
 namespace Basket.Tests
 {
-    public class Tests
+    public class BasketTests
     {
+        private Basket _basket;
+        
         [SetUp]
         public void Setup()
         {
+            _basket = new Basket();
         }
 
         [Test]
