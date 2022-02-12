e005b4f5c3d2852650a64bcf4f435418e66d7768
Kiril Rupasov <kirilrupasov@gmail.com>
2022-02-12 16:04:03 +0000

    added another two unit tests - one fails!

--- Basket.Tests/BasketTests.cs
+++ Basket.Tests/BasketTests.cs
@@ -54,6 +54,26 @@ namespace Basket.Tests
         }
 
         [Test]
+        public void WhenBasketHasTenMilk_ThenTotalPriceShouldBeNineTwenty()
+        {
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            _basket.AddToCart(new Milk());
+            
+            var expectedTotalPrice = 9.20M;
+            var actualTotalPrice = _basket.GetTotalCost();
+            
+            Assert.AreEqual(expectedTotalPrice, actualTotalPrice);
+        }
+
+        [Test]
         public void WhenBasketHasTwoButterOneBreadEightMilk_ThenTotalPriceShouldBeNine()
         {
             _basket.AddToCart(new Butter());
@@ -86,6 +106,23 @@ namespace Basket.Tests
             
             
             var expectedTotalPrice = 4.20M;
+            var actualTotalPrice = _basket.GetTotalCost();
+            
+            Assert.AreEqual(expectedTotalPrice, actualTotalPrice);
+        }
+        
+        [Test]
+        public void WhenBasketHasFourButterThreeBread_ThenTotalPriceShouldBeFiveTwenty()
+        {
+            _basket.AddToCart(new Butter());
+            _basket.AddToCart(new Butter());
+            _basket.AddToCart(new Butter());
+            _basket.AddToCart(new Butter());
+            _basket.AddToCart(new Bread());
+            _basket.AddToCart(new Bread());
+            _basket.AddToCart(new Bread());
+
+            var expectedTotalPrice = 5.20M;
             var actualTotalPrice = _basket.GetTotalCost();
             
             Assert.AreEqual(expectedTotalPrice, actualTotalPrice);
